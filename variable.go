94e5ef7b944404e9db87267e642fbef46cf51aa8
GeraAnggaraPutra <gera_098@smkassalaambandung.sch.id>
2023-05-04 11:20:07 +0700

    string

---
+++ variable.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Gera Anggara"
+	fmt.Println(name)
+
+	name = "Anggara"
+	fmt.Println(name)
+
+	var kota = "Bandung"
+	fmt.Println(kota)
+
+	var age = 17
+	fmt.Println(age)
+
+	country := "Indonesia" // deklarasi variable tanpa var
+	fmt.Println(country)
+
+	var (
+		firstName = "Gera"
+		lastName = "Anggara"
+	)
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
