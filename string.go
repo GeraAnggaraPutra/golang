5972808ae398acd45d20ea810954ea84175e85e5
GeraAnggaraPutra <gera_098@smkassalaambandung.sch.id>
2023-05-04 11:07:04 +0700

    string

---
+++ string.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Gera Anggara")
+	fmt.Println(len("Gera")) // menghitung jumlah karakter di string
+	fmt.Println("Gera Anggara"[0]) // mengambil karakter pada posisi yang ditentukan
+	fmt.Println("Gera Anggara"[1])
+}
