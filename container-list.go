09ad518243541f37a1625cc9f5c6a9a7394c70eb
GeraAnggaraPutra <gera_098@smkassalaambandung.sch.id>
2023-05-06 09:53:30 +0700

    package container-list

---
+++ container-list.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+)
+
+func main() {
+	data := list.New()
+
+	data.PushBack("Gera")
+	data.PushBack("Anggara")
+	data.PushBack("Putra")
+	data.PushFront("Khris")
+
+	// dari depan ke belakang
+	for element := data.Front(); element != nil; element = element.Next() {
+		fmt.Println(element.Value)
+	}
+
+	fmt.Println("====================================")
+
+	// dari belakang ke depan
+	for element := data.Back(); element != nil; element = element.Prev() {
+		fmt.Println(element.Value)
+	}
+}
